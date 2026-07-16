72e5e664f6b33c73ce3b23f1ff7006a9b2377e70
Ubuntu <ubuntu@ip-172-31-7-15.ap-south-1.compute.internal>
2026-07-16 12:11:41 +0000

    config .py setup from ec2

--- rms_backend/app/config.py
+++ rms_backend/app/config.py
@@ -51,6 +51,7 @@ def allowed_frontend_origins() -> List[str]:
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://rms-2-1.onrender.com',
+	'https://rms.raphaaa.com',
     }
     if settings.frontend_base_url:
         origins.add(settings.frontend_base_url.rstrip('/'))
